90154ef77ccc36ae2d6405665f7e89ef3b646e11
MangozTechnology <mangoztechnology@gmail.com>
2025-10-28 10:06:01 -0500

    Update sconpression.h

--- cpp-sconpression/sconpression.h
+++ cpp-sconpression/sconpression.h
@@ -5,4 +5,15 @@
 #ifndef _SCONPRESSION_H_
 #define _SCONPRESSION_H_
 
+#define SCONPRESSION_VERSION "1.0"
+
+class Sconpression {
+public:
+  Sconpression();
+  ~Sconpression();
+
+  /*sconpression version info print, calls the version macro*/
+  const auto _scnprs_str_info = SCONPRESSION_VERSION;
+};
+
 #endif
